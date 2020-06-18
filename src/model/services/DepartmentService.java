d6c18f6f3343d68da9067047f79b4a9c0e845687
Luciano Alves Araujo <lucianoalaraujo@yahoo.com.br>
2020-06-18 16:54:38 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -10,10 +10,17 @@ public class DepartmentService {
 	
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
-	public List<Department> findAll(){
-		
-		return dao.findAll();
+	public List<Department> findAll(){		
+		return dao.findAll();	
+	}
 	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
 	}
 
 }
